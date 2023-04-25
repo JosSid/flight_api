3fd00b8644490a20d0f0cb853482085e3349561a
Josep Valls <alfredvays@gmail.com>
2023-04-25 12:53:14 +0200

    PUT and DELETE methods

--- index.php
+++ index.php
@@ -14,6 +14,15 @@ Flight::route('GET /alumnos', function () {
     Flight::json($datos);
 });
 
+Flight::route('GET /alumnos/@id', function ($id) {
+    $sentencia = Flight::db()->prepare("SELECT * FROM `alumnos` WHERE id=?");
+    $sentencia->bindParam(1,$id);
+    $sentencia->execute();
+    $datos=$sentencia->fetch();
+    
+    Flight::json($datos);
+});
+
 Flight::route('POST /alumnos', function () {
 
     $nombre=(Flight::request()->data->nombre);
@@ -26,6 +35,33 @@ Flight::route('POST /alumnos', function () {
     $sentencia->execute();
     
     Flight::jsonp(['Alumno agregado']);
+});
+
+Flight::route('PUT /alumnos', function () {
+
+    $id=(Flight::request()->data->id);
+    $nombre=(Flight::request()->data->nombre);
+    $apellidos=(Flight::request()->data->apellidos);
+
+    $sql="UPDATE alumnos SET nombre=?, apellidos=? WHERE id=?";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(3,$id);
+    $sentencia->bindParam(1,$nombre);
+    $sentencia->bindParam(2,$apellidos);
+    $sentencia->execute();
+    
+    Flight::jsonp(['Alumno Modificado']);
+});
+
+Flight::route('DELETE /alumnos', function () {
+
+    $id=(Flight::request()->data->id);
+    $sql="DELETE FROM alumnos WHERE id=?";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(1,$id);
+    $sentencia->execute();
+
+    Flight::jsonp(['Alumno Borrado']);
 });
 
 Flight::start();
